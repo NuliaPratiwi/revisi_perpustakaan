39d2b06316200985211299a5b6851a2b85072cbd
jeniaandini <putujeniaandini@gmail.com>
2020-04-28 10:18:47 +0800

    Menambah modal pinjam buku

--- app/views/perpustakaan/peminjaman/index.php
+++ app/views/perpustakaan/peminjaman/index.php
@@ -1,6 +1,12 @@
 <!-- content -->
                       <div class="wrapper">
                         <div class="container">
+                          <!-- add Peminjaman -->
+        <div class="d-flex justify-content-end t-scale">
+            <a href="#popup">
+                <button class="btn btn-primary mb-3 shadow-lg">Pinjam Buku &nbsp;<i class="fas fa-plus"></i></button>
+            </a>
+        </div>
                           <div class="card">
                             <div class="card-header">
                               <h6 class="m-0 font-weight-bold text-primary"><i class="fas fa-book"></i> &nbsp;Daftar Peminjaman </h6>
@@ -36,3 +42,49 @@
                         </div>
                       </div>
                       <!-- end conten -->
+<div class="popup-wrapper" id="popup">
+    <div class="popup-container">
+        <div class="modal-header">
+            <h5 class="modal-title" id="judul">Pinjam Buku</h5>
+            <a href="#closed" class="close">&times;</a>
+        </div>
+        <form action="" method="POST" class="popup-form">
+            <div class="input-group row">
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3 label">
+                    <label for="nama">Nama Peminjam</label>
+                </div>
+                <select name="kategori" id="kategori" class="form-control-select">
+                        <option value="">Pilih Peminjam</option>
+                </select>
+            </div>
+            <!-- end input -->
+
+            <div class="input-group row">
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3 label">
+                    <label for="pengarang">Buku</label>
+                </div>
+                <select name="kategori" id="kategori" class="form-control-select">
+                        <option value="">Pilih Buku</option>
+                </select>
+            </div>
+            <!-- end input -->
+
+            <div class="input-group row">
+                <div class="col-xl-3 col-lg-3 col-md-3 col-sm-3 col-xs-3 label">
+                    <label for="kategori">Pinjam Berapa Hari</label>
+                </div>
+                
+                <select name="kategori" id="kategori" class="form-control-select">
+                        <option value="">Pilih Berapa Hari</option>
+                </select>
+               
+            </div>
+            <!-- end input -->
+
+            <div class="modal-footer">
+                <button type="submit" name="login" class="btn btn-primary w-100">Pinjam Buku <i class="fas fa-plus"></i></button>
+            </div>
+        </form>
+    </div>
+</div>
+<!-- end conten -->
