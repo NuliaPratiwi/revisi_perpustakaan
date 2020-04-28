fa3a28db8887d18574e85117042e64bb510ef8d0
NuliaPratiwi <60087465+NuliaPratiwi@users.noreply.github.com>
2020-04-28 10:55:49 +0800

    Membuat Laporan Buku

--- app/controllers/Laporan.php
+++ app/controllers/Laporan.php
@@ -6,9 +6,16 @@ class Laporan extends Controller
 
     public function index(){
         $data['judul'] = "Daftar Buku | SMKN 1 DENPASAR";
-        $this->view('template/header',$data);
+        $this->view('template/header',$data['judul'] = "laporan buku");
         $this->view('perpustakaan/laporan/index',$data);
         $this->view('template/footer');
     }
     
+
+    public function L_buku(){
+    	$data['judul'] = "laporan buku";
+    	$this->view('template/header',$data);
+    	$this->view('perpustakaan/laporan/L_buku', $data);
+	    $this->view('template/footer');
+    }
 }
